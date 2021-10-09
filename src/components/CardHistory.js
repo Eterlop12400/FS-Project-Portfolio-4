3f274091fbfc5ebdba55b85005dbbed0909f2c00
Eric Terlop <ETerlop@student.fullsail.edu>
2021-10-09 05:17:22 -0400

    created component and added styling with css modules

--- src/components/CardHistory.js
+++ src/components/CardHistory.js
@@ -4,9 +4,52 @@ function CardHistory(props) {
 
     return (
         <div style={styles.container}>
-            <h2 style={styles.body}>This is the Search History page!</h2>
+            <div src={props.imgContainer}>
+                <img src={props.img} alt={props.imgAlt} style={styles.img} />
+            </div>
+
+            <div style={styles.nameContainer}>
+                <h2 style={styles.cardName}>{props.cardName}</h2>
+            </div>
         </div>
     );
 }
 
 export default CardHistory;
+
+// CSS Modules
+const styles = {
+    container: {
+        width: '396px',
+        backgroundColor: '#C7DFFC',
+        height: '180px',
+        borderRadius: '4px',
+        display: 'flex',
+        alignItems: 'center',
+        marginBottom: '30px',
+        justifyContent: 'center',
+        boxShadow: '0px 3px 15px rgba(0,0,0,0.1)',
+        fontFamily: 'casablanca-urw, sans-serif',
+    },
+    imgContainer: {
+        width: '50%',
+    },
+    img: {
+        width: '100px',
+        height: '146px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
+    nameContainer: {
+        width: '50%',
+    },
+    cardName: {
+        marginLeft: '20px',
+        textOverflow: 'ellipsis',
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        textAlign: 'center',
+        fontWeight: '500',
+        fontSize: '28px',
+    }
+}
