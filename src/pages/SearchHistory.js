32554ddff99e053feb1185e061a9830ea69f888e
Eric Terlop <ETerlop@student.fullsail.edu>
2021-10-09 05:17:51 -0400

    Added styling and layout of page

--- src/pages/SearchHistory.js
+++ src/pages/SearchHistory.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+
+import CardHistory from "../components/CardHistory";
 
 function SearchHistory() {
 
+    const [searchHistory, setSearchHistory] = useState(JSON.parse(localStorage.getItem('cardSearchHistory')) || []);
+
+    let history = searchHistory.map((element, i) => {
+        return <CardHistory cardName={searchHistory[i].cardName} img={searchHistory[i].cardImg} key={i} />
+    })
+
     return (
         <main style={styles.container}>
-            <h2 style={styles.body}>This is the Search History page!</h2>
+            <header style={styles.headerContainer}>
+                <h2 style={styles.headerTxt}>Search History</h2>
+            </header>
+
+            <div style={styles.p}>
+                <p>
+                    Below are the latest unique cards searched (Max 6).
+                </p>
+            </div>
+
+            <section style={styles.contentContainer}>
+                {history}
+            </section>
         </main>
     );
 }
@@ -16,10 +36,33 @@ const styles = {
         width: '75%',
         backgroundColor: '#FDF5D9',
         margin: '0px auto',
-        height: '800px',
+        minHeight: '800px',
+        height: 'auto',
+        fontFamily: 'casablanca-urw, sans-serif',
     },
-    body: {
+    headerTxt: {
         textAlign: 'center',
         marginTop: '0',
+        fontWeight: '500',
+        fontSize: '32px',
+        color: '#071D3B',
+    },
+    p: {
+        width: '50%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginBottom: '50px',
+        textAlign: 'center',
+        fontWeight: '300',
+        fontSize: '18px',
+        color: '#4E574F',
+    },
+    contentContainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+    },
+    headerContainer: {
+        paddingTop: '50px',
     },
 }
