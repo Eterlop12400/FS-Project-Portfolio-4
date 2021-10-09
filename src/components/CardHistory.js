b1d3eb1bc3dd6d837d48ecdf5c343f2a983b7ac8
Eric Terlop <ETerlop@student.fullsail.edu>
2021-10-09 03:58:36 -0400

    Inital commit for component

---
+++ src/components/CardHistory.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CardHistory(props) {
+
+    return (
+        <div style={styles.container}>
+            <h2 style={styles.body}>This is the Search History page!</h2>
+        </div>
+    );
+}
+
+export default CardHistory;
