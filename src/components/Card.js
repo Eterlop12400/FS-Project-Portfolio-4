aee65aa1222ffff59673d1bb43c730e1c0b01779
Eric Terlop <ETerlop@student.fullsail.edu>
2021-10-10 02:55:38 -0400

    Replaces CSS module styling on some elements and replaced them with class names so we can style them in the css file based on screen width size

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,52 +3,52 @@ import React from 'react';
 function Card(props) {
     return (
         <div style={styles.wrapper}>
-            <section style={styles.container}>
-                <header style={styles.headerContainer}>
+            <section className='card--main-section-card-container'>
+                <header className='card--main-section-card-header'>
                     <h2 style={styles.headerTxt}>{props.name}</h2>
                 </header>
 
-                <section style={styles.section}>
-                    <div style={styles.sectionColumnOne}>
+                <section className='card--main-section-card-info'>
+                    <div className='card--main-section-card-column-img'>
                         <img style={styles.cardImg} src={props.image} alt={props.imgAlt}/>
                     </div>
 
-                    <section style={styles.sectionColumnTwo}>
-                        <section style={styles.halfRow}>
-                            <section style={styles.splitRow}>
+                    <section className='card--main-section-card-column-details'>
+                        <section className='half-row'>
+                            <section className='half-row--section'>
                                 <h3 style={styles.cardInfoTitle}>Attribute</h3>
                                 <p style={styles.cardInfoDetails}>{props.attribute}</p>
                             </section>
 
-                            <section style={styles.splitRow}>
+                            <section className='half-row--section'>
                                 <h3 style={styles.cardInfoTitle}>Level</h3>
                                 <p style={styles.cardInfoDetails}>{props.starLevel}</p>
                             </section>
                         </section>
 
-                        <section style={styles.row}>
+                        <section className='full-row--section'>
                             <h3 style={styles.cardInfoTitle}>Card Type</h3>
                             <p style={styles.cardInfoDetails}>{props.type}</p>
                         </section>
 
-                        <section style={styles.row}>
+                        <section className='full-row--section'>
                             <h3 style={styles.cardInfoTitle}>Sub Type</h3>
                             <p style={styles.cardInfoDetails}>{props.race}</p>
                         </section>
 
-                        <section style={styles.halfRow}>
-                            <section style={styles.splitRow}>
+                        <section className='half-row'>
+                            <section className='half-row--section'>
                                 <h3 style={styles.cardInfoTitle}>ATK</h3>
                                 <p style={styles.cardInfoDetails}>{props.atk}</p>
                             </section>
 
-                            <section style={styles.splitRow}>
+                            <section className='half-row--section'>
                                 <h3 style={styles.cardInfoTitle}>DEF</h3>
                                 <p style={styles.cardInfoDetails}>{props.def}</p>
                             </section>
                         </section>
 
-                        <section style={styles.effectTxt}>
+                        <section className='full-row--section-effect'>
                             <h3 style={styles.cardInfoTitle}>Card Text</h3>
                             <p style={styles.cardInfoDetails}>{props.description}</p>
                         </section>
@@ -71,13 +71,6 @@ const styles = {
         fontFamily: 'casablanca-urw, sans-serif',
         height: 'auto',
     },
-    headerContainer: {
-        height: '69px',
-        width: '705px',
-        borderRadius: '4px 4px 0 0',
-        backgroundColor: '#071D3B',
-        color: '#F6C443',
-    },
     headerTxt: {
         textAlign: 'center',
         fontSize: '32px',
@@ -85,46 +78,11 @@ const styles = {
         lineHeight: '69px',
         marginTop: '0',
     },
-    section: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-    },
-    sectionColumnOne: {
-        width: '40%',
-        marginTop: '10px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    sectionColumnTwo: {
-        width: '50%',
-        marginTop: '10px',
-    },
     cardImg: {
         width: '200px',
         height: '300px',
         marginTop: '0',
         alignSelf: 'center',
-    },
-    row: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        backgroundColor: '#FFFFFF',
-        borderRadius: '4px',
-        paddingLeft: '8px',
-        paddingRight: '8px',
-        height: '42px',
-        alignItems: 'center',
-        marginBottom: '5px',
-    },
-    effectTxt: {
-        backgroundColor: '#FFFFFF',
-        borderRadius: '4px',
-        minHeight: '42px',
-        height: 'auto',
-        padding: '1px 8px',
-        marginBottom: '10px',
     },
     cardInfoTitle: {
         fontWeight: '500',
@@ -140,22 +98,5 @@ const styles = {
         width: '100%',
         display: 'flex',
         justifyContent: 'center',
-    },
-    halfRow: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    splitRow: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        backgroundColor: '#FFFFFF',
-        borderRadius: '4px',
-        paddingLeft: '8px',
-        paddingRight: '8px',
-        height: '42px',
-        alignItems: 'center',
-        marginBottom: '5px',
-        width: '44%',
     },
 }
