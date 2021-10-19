bf0cef6d9a6db898cc6490441ceca5338b2ae29b
Eric Terlop <ETerlop@student.fullsail.edu>
2021-10-19 17:44:17 -0400

    Added a clear history button and added a multiple messages and button statuses based on the toggle state on if the search history is blank or not

--- src/pages/SearchHistory.js
+++ src/pages/SearchHistory.js
@@ -1,16 +1,26 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from 'react';
 
 // Importing Component
 import CardHistory from "../components/CardHistory";
 
 function SearchHistory() {
 
-    const [searchHistory] = useState(JSON.parse(localStorage.getItem('cardSearchHistory')) || []);
+    const [searchHistory, setSearchHistory] = useState(JSON.parse(localStorage.getItem('cardSearchHistory')) || []);
+
+    const [toggle, setToggle] = useState(true);
+
+    useEffect(() => {
+        if (searchHistory.length > 0) {
+            setToggle(true);
+        } else {
+            setToggle(false);
+        }
+    }, [searchHistory, toggle]);
 
     // Looping through the items in the searchHistory and making a CardHistory component for each one.
     let history = searchHistory.map((element, i) => {
         return <CardHistory cardName={searchHistory[i].cardName} img={searchHistory[i].cardImg} key={i} />
-    })
+    });
 
     return (
         <main className='search-history--main-container'>
@@ -18,17 +28,54 @@ function SearchHistory() {
                 <h2 style={styles.headerTxt}>Search History</h2>
             </header>
 
+            {/* If we have a search history that is not empty this message will appear */}
+            {toggle &&
             <div style={styles.p}>
                 <p>
                     Below are the latest unique cards searched (Max 6).
                 </p>
             </div>
+            }
+
+            {/* If we have a search history that is empty this message will appear */}
+            {!toggle &&
+            <div style={styles.p}>
+                <p>
+                    Search History is currently empty, please search for some cards!
+                </p>
+            </div>
+            }
+
+            {/* If we have a search history that is not empty this button will appear */}
+            {toggle &&
+            <div style={styles.btnContainer}>
+                <button style={styles.btn} onClick={clearHistory}>Clear Search History</button>
+            </div>
+            }
+
+            {/* If we have a search history that is empty this button will appear */}
+            {!toggle &&
+            <div style={styles.btnContainer}>
+                <button style={styles.disabledBtn} disabled>Clear Search History</button>
+            </div>
+            }
 
             <section style={styles.contentContainer}>
                 {history}
             </section>
         </main>
     );
+
+    /*
+    This function will make sure the toggle is true and if it is it will clear the local storage and
+    set the search history to a empty array.
+     */
+    function clearHistory() {
+        if (toggle === true) {
+            localStorage.clear();
+            setSearchHistory([]);
+        }
+    }
 }
 
 export default SearchHistory;
@@ -60,4 +107,29 @@ const styles = {
     headerContainer: {
         paddingTop: '50px',
     },
+    btn: {
+        padding: '20px 75px',
+        backgroundColor: '#071D3B',
+        color: '#FFC300',
+        fontSize: '23px',
+        fontWeight: '500',
+        borderRadius: '4px',
+        fontFamily: 'casablanca-urw, sans-serif',
+    },
+    disabledBtn: {
+        padding: '20px 75px',
+        backgroundColor: '#3c4e64',
+        color: '#FFC300',
+        fontSize: '23px',
+        fontWeight: '500',
+        borderRadius: '4px',
+        fontFamily: 'casablanca-urw, sans-serif',
+    },
+    btnContainer: {
+        width: '75%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginBottom: '50px',
+        textAlign: 'center',
+    }
 }
